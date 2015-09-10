588bfd00aa752d592990e8b9d751e70a7e5d8e12
Sensuke Osawa <senosa@gmail.com>
2015-09-10 16:03:53 +0900

    Show image from konachan.net

--- KonachanFrame/ViewController.swift
+++ KonachanFrame/ViewController.swift
@@ -10,10 +10,35 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var imageView: NSImageView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
 
-        // Do any additional setup after loading the view.
+    override func viewWillAppear() {
+        let url = NSURL(string: "http://konachan.net/post.json?limit=1&tags=order%3Arandom")
+        let req = NSURLRequest(URL: url!)
+        var resp: NSURLResponse?
+        var error: NSError?
+
+        var data = NSURLConnection.sendSynchronousRequest(req, returningResponse: &resp, error: &error)
+        if (error == nil) {
+            var json = NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.AllowFragments, error: &error) as! NSArray
+            if (error == nil) {
+                // ok
+                let item = json[0] as! NSDictionary
+                var jpegUrl = item.objectForKey("jpeg_url") as! String
+                let img = NSImage(contentsOfURL: NSURL(string: jpegUrl)!)
+                imageView.image = img
+            } else {
+                // json parse error
+                println(error)
+            }
+        } else {
+            // URL connection error
+            println(error)
+        }
     }
 
     override var representedObject: AnyObject? {
@@ -21,7 +46,5 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
-
 }
 
